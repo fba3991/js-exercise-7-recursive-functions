4f3eb1266a351bbe033756f36fc389565e8db708
Fabioes <fabionike@icloud.com>
2023-11-10 13:34:42 +0100

    refact

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/* 
   const persona = {
     nome: 'Luca',
     eta: 42,
@@ -31,3 +31,68 @@
     
   const conversione = convertiStringa(persona);
   console.log(conversione);
+ */
+
+ /*  /------------------------esercizio2-------------------------------/ */
+ /* return num === 1 ? 1 : num * calcolaFattoriale(num -1); */
+/*  const calcolaFattoriale = (num) => {
+  let fattoriale =1;
+  for(let i=num; i>0; i--){
+    fattoriale = fattoriale*i;
+  }
+  return fattoriale;
+ }  
+   
+    
+ 
+ 
+ 
+ window.addEventListener('load', () =>{
+
+    const inputUtente = document.querySelector('input');
+    const calcolaBtn = document.querySelector('button');
+    const prisultato = document.getElementById('risultato');
+
+    calcolaBtn.addEventListener('click',() => {
+      const numero = inputUtente.valueAsNumber;
+      const fattoriale = calcolaFattoriale(numero);
+    prisultato.innerText= `il fattoriale di${numero} e uguale a ${fattoriale}`;
+    });
+ }); */
+
+ /* -----------------esercizio3---------------------------------------------- */
+ const italy = {
+	nome: 'Repubblica Italiana',
+	bandiera: {
+		stile: 'verticale',
+		colori: ['rosso', 'verde', 'bianco'],
+		forma: 'rettangolo',
+	},
+	currency: 'euro',
+    	popolazione: 50000000,
+}
+
+const trasformObjIntoHTMLList=(obj) =>{
+    const ul = document.createElement('ul');
+     const keys = Object.keys(obj);
+     for(let i=0; i<keys.length; i++){
+        const key = keys[i];
+        const value = obj[key];
+        const li= document.createElement('li');
+        li.innerHTML=`${key}; `;
+        if((typeof value) !=='object'){
+            li.innerHTML += value;
+        }else{
+            const ul2 = trasformObjIntoHTMLList(value);
+            li.append(ul2);
+        }
+        ul.appendChild(li);
+       
+          }
+          return ul;
+        }
+     window.addEventListener('load', () =>{
+    const ul =trasformObjIntoHTMLList(italy);
+    document.body.appendChild(ul);
+    
+     });
